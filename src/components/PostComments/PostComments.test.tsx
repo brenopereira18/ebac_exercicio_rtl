5b2b72871576841c9026d3daacdd526d942f65a8
Breno Pereira <brenopereirabetti@gmail.com>
2024-08-13 11:50:03 -0300

    adicionando teste

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,27 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Deve adicionar dois comentários corretamente', () => {
+        render(<PostComment />);
+
+        const firstCommentText = 'Primeiro Comentário';
+        const secondCommentText = 'Segundo Comentário';
+
+        // Seleciona a textarea e o botão pelo atributo data-testid
+        const textarea = screen.getByTestId('comment-textarea');
+        const button = screen.getByTestId('submit-button');
+
+        // Simula a inserção do primeiro comentário
+        fireEvent.change(textarea, { target: { value: firstCommentText } });
+        fireEvent.click(button);
+
+        // Simula a inserção do segundo comentário
+        fireEvent.change(textarea, { target: { value: secondCommentText } });
+        fireEvent.click(button);
+
+        // Verifica se os dois comentários foram adicionados
+        expect(screen.getByText(firstCommentText)).toBeInTheDocument();
+        expect(screen.getByText(secondCommentText)).toBeInTheDocument();
+    });
 });
